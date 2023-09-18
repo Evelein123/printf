92d9ff131fb064a4acd4eaf58d85ef660ef5b166
Evelein123 <youssryeveleen@gmail.com>
2023-09-18 13:56:27 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,19 +1,27 @@
-#include <main.h>
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
  * _printf - A simplified printf.
- * @format: String with text and format specifiers.
- * It supports 'c': Character, 's': String, '%': Percent sign.
- * Return: The number of characters printed (excluding null byte).
+ * @format: Format string with text and specifiers.
+ *
+ * Writes the format string to stdout,
+ * replacing specifiers with additional arguments.
+ * Supports 'c' for Character, 's' for String, and '%' for Percent sign.
+ *
+ * Returns: Number of characters printed (excluding null byte).
  */
 
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int count = 0;
+	int i;
+	char *s;
 
 	va_start(args, format);
-	for (int i = 0; format[i]; i++)
+	for (i = 0; format[i]; i++)
 	{
 		if (format[i] != '%')
 		{
@@ -27,11 +35,16 @@ int _printf(const char *format, ...)
 			char c = (char)va_arg(args, int);
 
 			write(1, &c, 1);
-		} else if (format[i] == 's')
+		}
+		else if (format[i] == 's')
 		{
-			for (char *s = va_arg(args, char *); *s; s++)
-				write(1, s, 1);
-		} else if (format[i] == '%')
+			s = va_arg(args, char *);
+			while (*s)
+			{
+				write(1, s++, 1);
+			}
+		}
+		else if (format[i] == '%')
 		{
 			write(1, &format[i], 1);
 		}
@@ -40,3 +53,4 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (count);
 }
+
--- main.h
+++ main.h
@@ -1,6 +1,3 @@
-#ifndef _PRINTF_H
-#define _PRINTF_H
-
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -9,14 +6,6 @@
 #include <unistd.h>
 #include <limits.h>
 #include <stdlib.h>
-#include <stdout.h>
-
-int print_percent(va_list ap, params_t *params);
-int print_string(va_list ap, params_t *params);
-int print_char(va_list ap, params_t *params);
-int print_int(va_list ap, params_t *params);
-int print_s(va_list ap, params_t *params);
-
 
 
 
