b84e6403871be77b3ef578c737ce045f5b834274
Evelein123 <youssryeveleen@gmail.com>
2023-09-18 12:14:10 -0700

    printf

--- main.h
+++ main.h
@@ -11,6 +11,16 @@
 #include <stdlib.h>
 
 
+
+int print_percent(va_list ap, params_t *params);
+int print_string(va_list ap, params_t *params);
+int print_char(va_list ap, params_t *params);
+int print_int(va_list ap, params_t *params);
+int print_s(va_list ap, params_t *params);
+
+
+
+
 int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
