17268c13e9f6bfcb01b33de7f0b28460a9e7fcff
Evelein123 <youssryeveleen@gmail.com>
2023-09-18 11:20:16 -0700

    update

--- main.h
+++ main.h
@@ -1,17 +1,7 @@
-#ifndef _PRINTF_H
-#define _PRINTF_H
-
 #ifndef MAIN_H
 #define MAIN_H
 
 #include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <limits.h>
-#include <stdlib.h>
-
-
-#define NULL_STRING "(null)"
 
 int _printf(const char *format, ...);
 
