0e207d15d724506d5ad948af53ccffd5177d96d8
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-09-19 16:41:34 -0700

    a

--- _printf.c
+++ _printf.c
@@ -28,7 +28,7 @@ int _printf(const char *format, ...)
                 {
                 format++;
                 if (*format == '\0')
-                        return (-1);
+                        break;
                 if (*format == 'c')
                 {
                         char c = (char)va_arg(args, int);
