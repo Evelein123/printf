ecf2ed32054d1c938e1ab4fe45925f9f236c1c92
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-09-19 16:47:00 -0700

    a

--- _printf.c
+++ _printf.c
@@ -11,44 +11,43 @@
  */
 int _printf(const char *format, ...)
 {
-        va_list args;
-        int count = 0;
+	va_list args;
+	int count = 0;
 
-        if (format == NULL)
-                return (-1);
-        va_start(args, format);
-        while (*format)
-        {
-                if (*format != '%')
-                {
-                        write(1, format, 1);
-                        count++;
-                }
-                else
-                {
-                format++;
-                if (*format == '\0')
-                        break;
-                if (*format == 'c')
-                {
-                        char c = (char)va_arg(args, int);
-
-                        write(1, &c, 1);
-                        count++;
-                }
-                else if (*format == 's')
-                 count += printf_s(args);
-                else if (*format == 'd' || *format == 'i')
-                        count += printf_int(args);
-
-                else if (*format == '%')
-                {
-                        write(1, format, 1);
-                        count++;
-                }
+	if (format == NULL)
+		return (-1);
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			count++;
 		}
-                format++;
-        }
-        va_end(args);
-        return (count);
+		else
+		{
+		format++;
+		if (*format == '\0')
+			break;
+		if (*format == 'c')
+		{
+			char c = (char)va_arg(args, int);
+
+			write(1, &c, 1);
+			count++;
+		}
+		else if (*format == 's')
+			count += printf_s(args);
+		else if (*format == 'd' || *format == 'i')
+			count += printf_int(args);
+		else if (*format == '%')
+		{
+			write(1, format, 1);
+			count++;
+		}
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
